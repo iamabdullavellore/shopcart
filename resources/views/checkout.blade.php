48eb65082d8347082997133e09a2b41b76b6aa86
Abdul Rahman <abdullavellore@gmail.com>
2019-01-16 19:03:24 +0530

    Update checkout.blade.php

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -50,13 +50,13 @@
 									<div class="col-xs-4">
 										<input type="text" class="form-control input-sm" value="{{$Products->quantity}} Nos" disabled>
 									</div>
-									<div class="col-xs-2">
+									<!-- <div class="col-xs-2">
 										<a href="{{ url('checkout/delete', $Products->id) }}">
 											<button type="button" class="btn btn-link btn-xs">
 												<span class="glyphicon glyphicon-trash"> </span>
 											</button>
 										</a>
-									</div>
+									</div> -->
 								</div>
 							</div>
 							@endforeach
@@ -64,7 +64,7 @@
 					<hr>	
 
 					<hr>
-					<div class="row">
+					<!-- <div class="row">
 						<div class="text-center">
 							<div class="col-xs-9">
 								<h6 class="text-right">Added items?</h6>
@@ -75,7 +75,7 @@
 								</button>
 							</div>
 						</div>
-					</div>
+					</div> -->
 				</div>
 				<div class="panel-footer">
 					<div class="row text-center">
